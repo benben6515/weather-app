f1ba84ff88edd680a63b4115127937c1f95e4198
Benben <z2266109@gmail.com>
2022-06-10 00:10:46 +0800

    add: `.env.smple` file

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    open: true,
     proxy: {
       '/api-geo': {
         target: 'https://api.openweathermap.org/geo/1.0',

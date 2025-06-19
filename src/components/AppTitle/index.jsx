d5715b69a23c6ef15e6a2fafce4f6f54d9c87c72
benben <z2266109@gmail.com>
2025-06-19 10:12:12 +0800

    fix: clear up

--- src/components/AppTitle/index.jsx
+++ src/components/AppTitle/index.jsx
@@ -4,7 +4,11 @@ const AppTitle = () => {
   return (
     <header className="h-32">
       <div className="flex justify-center items-center">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
         <h1 className="text-4xl text-sky-300">Weather App</h1>
       </div>
       <p className="text-lg text-sky-700 text-shadow-md">Search weather at everywhere.</p>
